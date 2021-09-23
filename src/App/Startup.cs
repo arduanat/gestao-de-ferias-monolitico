0c4ea7920a944cb0b2f97983c6cf28944e30ce4f
Jônatas Legal <jonataslegal@gmail.com>
2021-09-23 11:31:36 -0400

    limpando código

--- src/App/Startup.cs
+++ src/App/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Aplicacao
+namespace App
 {
     public class Startup
     {
